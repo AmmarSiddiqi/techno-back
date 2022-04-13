28e887411580910177d1896ecd080a969d9048d3
Mehdishah57 <mehdi55shah@gmail.com>
2022-04-13 11:07:57 +0500

    11:15AM 4/13/2022

--- src/controllers/message/getMessage/getMessage.js
+++ src/controllers/message/getMessage/getMessage.js
@@ -6,7 +6,10 @@ const getMessage = handleRouteErrors(async (req, res) => {
 	const { id } = await validate(req.body);
 	const messages = await Message.findOne({_id: id, $or: [
 		{idOne:req.user._id},{idTwo:req.user._id}
-	]}).catch(() => null);
+	]})
+	.populate("idOne","name email image")
+    .populate("idTwo", "name email image")
+	.catch(() => null);
 	if (!messages) return res.status(404).send("Apparently you haven't sent any messages");
 	res.status(200).send(messages);
 })
--- src/start/envConfig.js
+++ src/start/envConfig.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 
 const { NODE_ENV } = process.env;
-NODE_ENV === "development" ?
+NODE_ENV.trim() === "development" ?
 dotenv.config({ debug: true, path: `.env.${NODE_ENV.trim()}` })
 : dotenv.config();
