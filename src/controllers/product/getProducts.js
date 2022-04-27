f278f49c9021bfd6c348f70a6dbfeb6d212c349c
Mehdishah57 <mehdi55shah@gmail.com>
2022-04-28 00:36:15 +0500

    12:45 AM 4/28/2022

--- src/controllers/product/getProducts.js
+++ src/controllers/product/getProducts.js
@@ -2,20 +2,33 @@ import { Product } from '../../models/product.js';
 import handleRouteErrors from './../../handleRouteErrors.js';
 
 const getProducts = handleRouteErrors(async (req, res) => {
-	const { search, pageNumber: pageNo } = req.body;
+	const { search, pageNumber: pageNo, filters } = req.body;
+	console.log(filters)
 	if (!pageNo)
 		return res.status(400).send("Please provide \"pageNumber\"");
 	let pageSize = 10;
 	let pageNumber = parseInt(pageNo);
 	if (pageNumber < 1) return res.status(400).send("Page Number can't be less than \"1\"");
 	if (search) {
-		const products = await Product.find({"$text":{"$search": search}, isActive:true})
+		const products = await Product.find({
+			"$text": { "$search": search }, isActive: true,
+			$and: [
+				{ price: { "$gte": filters['Minimum Price'] || Number.NEGATIVE_INFINITY } },
+				{ price: { "$lte": filters['Maximum Price'] || Number.POSITIVE_INFINITY } }
+			]
+		})
 			.select("title price city picture.image1 favourites owner location")
 			.populate("location")
 			.skip((pageNumber - 1) * pageSize).limit(pageSize);
 		return res.status(200).send(products);
 	}
-	const products = await Product.find({isActive:true})
+	const products = await Product.find({
+		isActive: true, $and: [
+			{ price: { "$gte": filters['Minimum Price'] || Number.NEGATIVE_INFINITY } },
+			{ price: { "$lte": filters['Maximum Price'] || Number.POSITIVE_INFINITY } }
+		]
+	})
+		.sort({ [filters['High to Low']]: -1, [filters['Low to High']]: 1 })
 		.select("title price city picture.image1 favourites owner location")
 		.populate("location")
 		.skip((pageNumber - 1) * pageSize).limit(pageSize);
--- src/controllers/user/signup.js
+++ src/controllers/user/signup.js
@@ -1,30 +1,21 @@
 import handleRouteErrors from "../../handleRouteErrors.js";
 import { User, validate } from "../../models/user.js";
 import { genSalt, hash } from "bcrypt";
-import twilio from "twilio";
 
 const signup = handleRouteErrors(async(req,res)=>{
     const validated = await validate(req.body);
     let exists = await User.findOne({email: req.body.email});
     if(exists) return res.status(400).send("You've already registered");
     const user = new User(validated);
-    const phoneValidationKey = Math.floor(100000 + Math.random() * 900000);
-    const client = twilio(process.env.accountSid,process.env.authToken);
-    await client.messages.create({ 
-        messagingServiceSid: process.env.messagingServiceSid,
-         body: `Your verification key is: ${phoneValidationKey}`,        
-         to: `${user.countryCode + user.phoneNumber}` 
-    });
     const salt = await genSalt(10);
     const hashedPass = await hash(user.password,salt);
-    const hashedPhoneKey = await hash(phoneValidationKey.toString(),salt);
     const verifyHash = await hash(process.env.falseSecret,salt);
     user.password = hashedPass;
-    user.phoneValidationKey = hashedPhoneKey;
     user.verified = verifyHash;
     await user.save();
     const token = await user.genAuthToken();
     let userToSend = {
+        _id:user._id,
         name: user.name,
         email: user.email, 
         countryCode: user.countryCode, 
