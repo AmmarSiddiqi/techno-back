b3b4df6ca3b8734237f7140f0b9cd6f0d950450a
Mehdishah57 <mehdi55shah@gmail.com>
2022-05-26 10:36:56 +0500

    10:45 AM 5/26/2022

--- src/controllers/product/updateProduct.js
+++ src/controllers/product/updateProduct.js
@@ -1,5 +1,27 @@
+import { Product } from '../../models/product.js';
 import handleRouteErrors from './../../handleRouteErrors.js';
+import cloudinary from "../../cloudinary/cloudinary.js";
 
-const updateProduct = handleRouteErrors(async(req,res)=>{});
+const updateProduct = handleRouteErrors(async(req,res)=>{
+    let {_id, title, price, description, picture} = req.body;
+    let uploadPics = {}
+    Object.keys(picture).map(key => {
+        if(typeof picture[key] === "string")
+            uploadPics[key] = picture[key]
+    })
+    const imagesTags = Object.keys(uploadPics);
+    for(let i=0; i< imagesTags.length; i++){
+        const { url, public_id } = await cloudinary.uploader.upload(uploadPics[imagesTags[i]],
+            {
+            upload_preset:'dev_setups',
+        });
+        if(url) picture[imagesTags[i]] = { url, public_id }
+        else if(error) 
+            return res.status(500).send({message:"There was an error uploading images",error})
+    }
+    const {modifiedCount} = await Product.updateOne({_id, owner: req.user._id},
+        {title, price, description, picture})
+    res.status(200).send({message: modifiedCount});
+});
 
 export default updateProduct;
--- src/controllers/user/userPicture.js
+++ src/controllers/user/userPicture.js
@@ -5,13 +5,13 @@ import cloudinary from "../../cloudinary/cloudinary.js";
 const userPicture = handleRouteErrors(async (req, res) => {
     const { image } = req.body;
     if (!image) return res.status(400).send("No Image was provided");
-    const { url } = await cloudinary.uploader.upload(image,{
+    const { url, public_id } = await cloudinary.uploader.upload(image,{
         upload_preset:'dev_setups',
     });
     const user = await User.findById(req.user._id);
-    user.image = { url };
+    user.image = { url, id: public_id };
     await user.save();
-    res.status(200).send({ url });
+    res.status(200).send({ url, public_id });
 })
 
 export default userPicture;
